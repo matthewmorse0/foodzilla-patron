cec7abea2d9287b1ba355d544c67c7dfa410f973
NathanR4 <ramireznathan440@gmail.com>
2022-10-23 14:14:27 -0500

    base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestaurantCardComponent } from './restaurant-card/restaurant-card.component';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/restaurant-card/restaurant-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+@Component({
+  selector: 'app-restaurant-card',
+  templateUrl: './restaurant-card.component.html',
+  styleUrls: ['./restaurant-card.component.css']
+})
+export class RestaurantCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
