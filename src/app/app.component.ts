b30e78fe0e1e4aad95656478922300999615dc2c
NathanR4 <ramireznathan440@gmail.com>
2022-10-26 18:35:59 -0500

    API Call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { firstValueFrom, Observable } from 'rxjs';
 import { RestaurantCardComponent } from './restaurant-card/restaurant-card.component';
-
+import { RestaurantObject } from 'src/assets/restObject';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  response: RestaurantObject[];
+  rest1: RestaurantObject;
+
   title = 'foodzilla-patron';
+  constructor(private http: HttpClient) {}
+
+  async ngOnInit () {
+    this.getRests();
+  }
+  
+  getConfigResponse(): Observable<RestaurantObject> {
+    return this.http.get<RestaurantObject>(
+      "http://127.0.0.1:5000/get_all_info")
+  }
+
+
+  private async getRests() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://127.0.0.1:5000/get_all_info");
+    xhr.send();
+    xhr.onload = () => 
+    {
+      var data = xhr.responseText;
+      var response = JSON.parse(data)
+      this.rest1 = response.restaurants[0];
+      //console.log(this.rest1);
+    }
+  }
+
 }
+
--- src/app/restaurant-card/restaurant-card.component.ts
+++ src/app/restaurant-card/restaurant-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { RestaurantObject } from 'src/assets/restObject';
 
 @Component({
   selector: 'app-restaurant-card',
@@ -8,9 +9,30 @@ import { MatExpansionModule } from '@angular/material/expansion';
 })
 export class RestaurantCardComponent implements OnInit {
 
-  constructor() { }
+  rest1: RestaurantObject;
+  title = 'foodzilla-patron';
 
-  ngOnInit(): void {
+  async ngOnInit () {
+    this.getRests();
   }
 
+  private async getRests() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://127.0.0.1:5000/get_all_info");
+    xhr.send();
+    xhr.onload = () => 
+    {
+      var data = xhr.responseText;
+      var response = JSON.parse(data)
+      this.rest1 = response.restaurants[0] as RestaurantObject
+      this.rest1.name = this.rest1[0].toString();
+      this.rest1.id = this.rest1[1]
+      this.rest1.managerId = this.rest1[2]
+      this.rest1.waitTime = this.rest1[3]
+      this.rest1.menuLink = this.rest1[4]
+      this.rest1.diningTables = this.rest1[5]
+      this.rest1.freeTables = this.rest1[6]
+      console.log(this.rest1);
+    }
+  }
 }
